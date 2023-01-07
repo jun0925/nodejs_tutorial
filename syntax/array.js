bbf372d22a15ba43a4ed51cf32e08a2ae031a833
yeongjun_kim <yeongjun0925@gmail.com>
2023-01-07 15:04:18 +0900

    배열

---
+++ syntax/array.js
@@ -0,0 +1,20 @@
+// CRUD
+var arr = ['A','B','C','D'];
+
+// Read
+console.log(arr[1]);
+console.log(arr[3]);
+
+// Update
+arr[2] = 3;
+console.log(arr);
+
+console.log(arr.length);
+
+// Create 맨 뒤에 추가
+arr.push('E');
+console.log(arr);
+
+// Delete 맨 뒤에서 제거
+arr.pop();
+console.log(arr);
