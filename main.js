606552387d404a249ac82ba7591e17819a1e0b36
yeongjun_kim <yeongjun0925@gmail.com>
2023-01-07 14:48:59 +0900

    조건문을 사용해서 루트 페이지 기능 구현

--- main.js
+++ main.js
@@ -6,32 +6,61 @@ var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     if(pathname === '/') {
-      // 파일 읽기
-      fs.readFile(`data/${title}`, 'utf-8', function (err, description) {
-        var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+      if(queryData.id === undefined) {
+        // 파일 읽기
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `<!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          console.log(template);
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        // 파일 읽기
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+          var title = queryData.id;
+          var template = `<!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
     } else {
       response.writeHead(404);
       response.end('Not Found');
