1962e050b460c5eb0aaea50fd24215a6d7e31384
yeongjun_kim <yeongjun0925@gmail.com>
2023-01-10 09:26:47 +0900

    글수정-파일명 변경, 내용저장

--- main.js
+++ main.js
@@ -127,6 +127,24 @@ var app = http.createServer(function(request, response){
           response.end(template);
         });
       });
+    } else if (pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+      });
+      request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        console.log(post);
+        fs.rename(`data/${id}`, `data/${title}`, function () {
+          fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');
+          });
+        })
+      });
     } else {
       response.writeHead(404);
       response.end('Not Found');
