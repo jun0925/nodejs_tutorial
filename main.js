51eaab6131ce81cb6b001ae07a1ea5d850335ea9
yeongjun_kim <yeongjun0925@gmail.com>
2023-01-12 08:22:08 +0900

    삭제 기능 구현

--- main.js
+++ main.js
@@ -59,7 +59,14 @@ var app = http.createServer(function(request, response){
             var list = templateList(filelist);
             var template = templateHTML(title, list, 
               `<h2>${title}</h2><p>${description}</p>`, 
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+              `<a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="post" onsubmit="return confirm('Are You Delete?")">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>
+              `
+              );
             response.writeHead(200);
             response.end(template);
           });
@@ -137,13 +144,25 @@ var app = http.createServer(function(request, response){
         var id = post.id;
         var title = post.title;
         var description = post.description;
-        console.log(post);
         fs.rename(`data/${id}`, `data/${title}`, function () {
           fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end('success');
           });
         })
+      });
+    } else if (pathname === '/delete_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+      });
+      request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function (error) {
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        });
       });
     } else {
       response.writeHead(404);
